76193f9c89babdca5919da63ff82bbb0ac7919ad
NotAlwaysTrue <2136846186@qq.com>
2024-11-29 14:44:52 +0800

    Code Compeletion

--- lua/Scripts/Shared/Shared.lua
+++ lua/Scripts/Shared/Shared.lua
@@ -1,22 +1,25 @@
 activeCharge = { }
 
-Hook.Add("roundStart()", "Drones.resettable", function()
-    if activeCharge == nil then return end
+Hook.Add("roundEnd", "Charge.resetonend", function()
+    for i,v in pairs(activeCharge) do
+        local light = i.GetComponentString("LightComponent")
+        light.isOn = false
+    end
     activeCharge = { }
 end)
 
-Hook.Add("character.death", "Drones.resetOndead", function(character)  --Reset on death
-    if activeCharge == nil then return end
+Hook.Add("character.death", "Charge.resetOndead", function(character)  --Reset on death
+    if activeCharge == nil or character == nil then return end
     for i,v in pairs(activeCharge) do
         if v == character then
             local light = i.GetComponentString("LightComponent")
             light.isOn = false
-            i.remove()
+            activeCharge[i] = nil
         end
     end
 end)
 
-Hook.Add("item.use", "Drones.itemused", function(item, usingCharacter)
+Hook.Add("item.use", "Charge.itemused", function(item, usingCharacter)
     if item == nil or  usingCharacter == nil then return end
     local identifier = item.Prefab.Identifier.Value
     local methodtorun = KACBRN.ItemMethods[identifier]
@@ -33,6 +36,7 @@ KACBRN.ItemMethods.m57detonator = function(item, usingCharacter)
     for i,v in pairs(activeCharge) do
         if v == usingCharacter then
             i.Condition = 0
+            table.remove(activeCharge, i)
         end
     end
 end
