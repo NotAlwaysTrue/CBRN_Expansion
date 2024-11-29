8563e7b7bddc49db166d9ed5fc6a4e09ae7557ec
NotAlwaysTrue <2136846186@qq.com>
2024-11-29 09:56:12 +0800

    Updated codes allow remove and reset

--- lua/Autorun/init.lua
+++ lua/Autorun/init.lua
@@ -1,6 +1,4 @@
 KACBRN = {}
 KACBRN.Path = table.pack(...)[1]
 
-if true then
-	dofile(KACBRN.Path.."/lua/Scripts/Shared/Shared.lua")
-end
+dofile(KACBRN.Path.."/lua/Scripts/Shared/Shared.lua")
--- lua/Scripts/Shared/Shared.lua
+++ lua/Scripts/Shared/Shared.lua
@@ -1,5 +1,21 @@
 activeCharge = { }
 
+Hook.Add("roundStart()", "Drones.resettable", function()
+    if activeCharge == nil then return end
+    activeCharge = { }
+end)
+
+Hook.Add("character.death", "Drones.resetOndead", function(character)  --Reset on death
+    if activeCharge == nil then return end
+    for i,v in pairs(activeCharge) do
+        if v == character then
+            local light = i.GetComponentString("LightComponent")
+            light.isOn = false
+            i.remove()
+        end
+    end
+end)
+
 Hook.Add("item.use", "Drones.itemused", function(item, usingCharacter)
     if item == nil or  usingCharacter == nil then return end
     local identifier = item.Prefab.Identifier.Value
@@ -13,7 +29,7 @@ end)
 KACBRN.ItemMethods = {}
 
 KACBRN.ItemMethods.m57detonator = function(item, usingCharacter)
-    if usingCharacter == nil or next(activeCharge) == nil then return end
+    if usingCharacter == nil or activeCharge == nil then return end
     for i,v in pairs(activeCharge) do
         if v == usingCharacter then
             i.Condition = 0
