c8b5ae6bb406d2586e3883f2acafc5ee47972cf3
NotAlwaysTrue <2136846186@qq.com>
2024-12-17 22:37:39 +0800

    Updated Lua works, does not affact anything

--- lua/Autorun/init.lua
+++ lua/Autorun/init.lua
@@ -1,22 +1,25 @@
 JZ_CBRN = {}
 JZ_CBRN.Path = table.pack(...)[1]
 
-dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/Shared.lua")
 
 if Game.IsMultiplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/Shared.lua")
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/DronesShared.lua")
+end
+
+if SERVER then
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/C4Shared.lua")
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Server/DroneServer.lua")
 end
 
 if CLIENT and Game.IsMultiplayer then
-	--dofile(JZ_CBRN.Path.."/lua/Scripts/Client/HUD.lua")  --Unfinished GUI works
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/Client.lua")
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/DroneClient.lua")
 end
 
-if SERVER and Game.IsMultiplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Server/Server.lua")
+if CLIENT then
+	--dofile(JZ_CBRN.Path.."/lua/Scripts/Client/HUD.lua")  --Unfinished GUI works
 end
 
 if Game.IsSingleplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/SP.lua")
-	--dofile(JZ_CBRN.Path.."/lua/Scripts/Client/HUD.lua")  --Unfinished GUI works
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/C4Shared.lua")
+	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/DroneSP.lua")
 end
--- lua/Scripts/Shared/C4Shared.lua
+++ lua/Scripts/Shared/C4Shared.lua
@@ -1,8 +1,4 @@
-LuaUserData.MakeFieldAccessible(Descriptors["Barotrauma.StatusEffect"],"user")
 activeCharge = { }
-Controllers = { }
-
---Hook Part
 
 Hook.Add("roundEnd", "CBRN_Charge.resetonend", function() --Endround reset
     for i,v in pairs(activeCharge) do
@@ -32,36 +28,6 @@ Hook.Add("item.use", "CBRN_Charge.itemused", function(item, usingCharacter)
         return
     end
 end)
-
-Hook.Add("Drone.control", "Drone.control", function(effect, deltaTime, item, targets, worldPosition)
-    if targets == nil then return end
-    local targetCharacter = targets[1]
-    if targetCharacter == nil or targetCharacter.IsPlayer or tostring(targetCharacter.SpeciesName) ~= "Cbrn_mech" then return end
-    if effect.type == ActionType.OnUse then
-        if SERVER then
-            local usingCharacter = effect.user
-            local usingclient = Util.FindClientCharacter(usingCharacter)
-            if usingCharacter == nil or usingclient == nil then return end
-            if not targetCharacter.IsFriendly(usingCharacter) then
-                local message = Networking.Start("notallowed")
-                message.WriteString(true)
-                Networking.Send(message, usingclient.Connection)
-                return
-            end
-            Controllers[usingclient] = usingCharacter
-            usingclient.SetClientCharacter(targetCharacter)
-            local message = Networking.Start("user")
-            message.WriteString(tostring(usingCharacter.ID))
-            Networking.Send(message, usingclient.Connection)
-        end
-        if CLIENT then
-            charCurrentControlling = targetCharacter
-            controlling = true
-        end
-    end
-end)
-
---Item Method
 
 JZ_CBRN.ItemMethods = {}
 
