96bf46195d2f012181ba1d0ea963fbe4adbb5932
NotAlwaysTrue <77662224+NotAlwaysTrue@users.noreply.github.com>
2025-01-01 03:24:34 +0800

    Delete old lua directory

--- lua/Autorun/assistfunctions.lua
+++
@@ -1,9 +0,0 @@
-function findcharacterbyID(id)
-    local characterlist = Character.CharacterList
-    for i,v in pairs(characterlist) do
-        if tostring(v.ID) == id then
-            return v
-        end
-    end
-    return nil
-end
