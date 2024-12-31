9fa473428f2058d3f6fdc28757fd4e45c40b2be1
NotAlwaysTrue <2136846186@qq.com>
2024-12-31 18:34:29 +0800

    V4.4.2 Fix Path issue in init.lua

--- lua/Autorun/init.lua
+++ lua/Autorun/init.lua
@@ -3,26 +3,26 @@ JZ_CBRN.Path = table.pack(...)[1]
 
 
 if Game.IsMultiplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/DronesShared.lua")
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Shared/DronesShared.lua")
 end
 
 if SERVER then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/C4Shared.lua")
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Server/DroneServer.lua")
-	dofile(JZ_CBRN.Path..'/lua/Scripts/Shared/MSL_GUIDE.lua')
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Shared/C4Shared.lua")
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Server/DroneServer.lua")
+	dofile(JZ_CBRN.Path..'/Lua/Scripts/Shared/MSL_GUIDE.lua')
 end
 
 if CLIENT and Game.IsMultiplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/DroneClient.lua")
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Client/DroneClient.lua")
 end
 
 if CLIENT then
-	--dofile(JZ_CBRN.Path.."/lua/Scripts/Client/HUD.lua")  --Unfinished GUI works
-	dofile(JZ_CBRN.Path..'/lua/Scripts/Client/TurretZoom.lua')
+	--dofile(JZ_CBRN.Path.."/Lua/Scripts/Client/HUD.lua")  --Unfinished GUI works
+	dofile(JZ_CBRN.Path..'/Lua/Scripts/Client/TurretZoom.lua')
 end
 
 if Game.IsSingleplayer then
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Shared/C4Shared.lua")
-	dofile(JZ_CBRN.Path..'/lua/Scripts/Shared/MSL_GUIDE.lua')
-	dofile(JZ_CBRN.Path.."/lua/Scripts/Client/DroneSP.lua")
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Shared/C4Shared.lua")
+	dofile(JZ_CBRN.Path..'/Lua/Scripts/Shared/MSL_GUIDE.lua')
+	dofile(JZ_CBRN.Path.."/Lua/Scripts/Client/DroneSP.lua")
 end
