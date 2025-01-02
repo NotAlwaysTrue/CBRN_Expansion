2d1d32b9b140986353c994fa11765b4188816f53
NotAlwaysTrue <2136846186@qq.com>
2025-01-02 22:47:20 +0800

    V4.4.3 Update Added SM-8 UTAM Modified LD-AGW Fabricate requirements Modified LD-AGW Explosion Damage Add a 1s safety for LD-AGW to avoid unexpected detenation Removed ability to detect dead bodies for LD-AGW Add Micro VLS and Loader Add Sonar headset Add AFCS scope and AFCS affliction Changed missile guidence system to use table instead of originally hard-coded Modified Client side Zoom Effect

--- Lua/Scripts/Client/TurretZoom.lua
+++ Lua/Scripts/Client/TurretZoom.lua
@@ -1,9 +1,9 @@
 Hook.Patch("Barotrauma.Character", "ControlLocalPlayer", function(instance, ptable)
     if not instance then return end
-    
-	-- todo: only zoom out when the character is guiding a missile
-    if instance.SelectedItem and instance.SelectedItem.HasTag("periscope") and instance.SelectedItem.HasTag("GUIDENCE_SYSTEM") then
+
+    if instance.SelectedItem and instance.SelectedItem.HasTag("GUIDENCE_SYSTEM") then --
 		Screen.Selected.Cam.MinZoom = 0.08
-        Screen.Selected.Cam.OffsetAmount = 2500
+        Screen.Selected.Cam.OffsetAmount = 3000
     end
 end, Hook.HookMethodType.After)
+
---
+++ Lua/Scripts/mslsettings.lua
@@ -0,0 +1,24 @@
+mslsettings = {
+
+    cbrn_SACLOS_msl = {
+        MAX_STEERING_FORCE = 0.02,       -- Steer the missile towards the direction the cursor is pointing at, smaller num means the msl will try harder to maneuver when going closer to LOS
+        MAX_CORRECTION_FORCE = 0.7,      -- Correct the missile flying path towards the LOS , larger num means the msl will try harder to go back to LOS
+        MAX_PROPULSION_FORCE = 0.15,     -- Accelerate the missile in its current direction
+        AVR_G_FORCE_MULTIPLIER = 3.0,    -- Decide how hard can the missile turn to LOS, larger means less maneuver ability
+        TOLERANCE = 0.05,                -- Decide how much error from LOS, 0(0%) means it will not follow LOS, 1(100%) means it will try its best to follow LOS
+        STEERAGE_MULTIPLIER = 0.80,      -- Decide how hard the missile can maneuver when in large , smaller num means less maneuver ability
+        INIT_LAUNCH_SPEED = 8,          -- Initial launch speed
+        DISABLE_RESISTANCE = false,      -- Will the missile take water resistance, Change this may lead to significant differences
+    },
+    std_8_utam = {
+        MAX_STEERING_FORCE = 0.016,
+        MAX_CORRECTION_FORCE = 0.85,
+        MAX_PROPULSION_FORCE = 0.25,
+        AVR_G_FORCE_MULTIPLIER = 2.0,
+        TOLERANCE = 0.02,
+        STEERAGE_MULTIPLIER = 0.90,
+        INIT_LAUNCH_SPEED = 15,
+        DISABLE_RESISTANCE = false,
+    }
+
+}
