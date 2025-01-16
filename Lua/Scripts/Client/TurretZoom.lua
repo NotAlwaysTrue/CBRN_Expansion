74b741bfc26b6da0f17c83f6bfbb4e9579ca0956
NotAlwaysTrue <2136846186@qq.com>
2025-01-16 20:50:22 +0800

    Modified AA-12 FRAG-12 Magazine to make it stronger but no longer refillable with ammobox Changed icon for triton_7L_71 Removed dup railgunjumppod Use new zoomkey instead of fixed turret zoom

--- Lua/Scripts/Client/TurretZoom.lua
+++ Lua/Scripts/Client/TurretZoom.lua
@@ -1,9 +1,59 @@
-Hook.Patch("Barotrauma.Character", "ControlLocalPlayer", function(instance, ptable)
-    if not instance then return end
+local zoom_init = false
 
-    if instance.SelectedItem and instance.SelectedItem.HasTag("GUIDENCE_SYSTEM") then --
-		Screen.Selected.Cam.MinZoom = 0.05
-        Screen.Selected.Cam.OffsetAmount = 8000
+local zoomSpeed=0.02
+local zoomMin=0.01 -- minimum zoom modifier
+local zoomMax=2.5 -- maximum zoom modifier
+local zoomStart=1.5 -- default zoom level
+
+
+local resetKey=Keys.Back -- reset zoom key
+
+local zoomStart=math.max(math.min(zoomMax,zoomStart),zoomMin)
+local globalzoomNew=zoomStart
+local globalzoomMin=zoomMin
+local globalzoomMax=zoomMax
+
+local globalzoomDefault
+local globalzoomDefaultMin
+local globalzoomDefaultMax
+
+
+LuaUserData.MakeFieldAccessible(Descriptors["Barotrauma.Camera"],"globalZoomScale")
+LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.Camera"],"CreateMatrices")
+
+Hook.HookMethod("Barotrauma.Camera","CreateMatrices",function(instance,ptable)
+	globalzoomDefault=instance.globalZoomScale
+	globalzoomDefaultMin=instance.MinZoom
+	globalzoomDefaultMax=instance.MaxZoom
+	globalzoomMin=math.max(zoomMin,globalzoomDefault*zoomMin)
+	globalzoomMax=math.min(zoomMax,globalzoomDefault*zoomMax)
+	globalzoomNew=math.max(math.min(globalzoomMax,globalzoomDefault*zoomStart),globalzoomMin)
+	instance.MinZoom=math.min(globalzoomMin/2,instance.MinZoom)
+	instance.MaxZoom=math.max(globalzoomMax*2,instance.MaxZoom)
+end,Hook.HookMethodType.After)
+
+Hook.HookMethod("Barotrauma.Character","ControlLocalPlayer",function(instance,ptable)
+    if not zoom_init then
+        ptable.cam.CreateMatrices()
+        zoom_init = true
     end
-end, Hook.HookMethodType.After)
+    if instance.SelectedItem and instance.SelectedItem.HasTag("GUIDENCE_SYSTEM") then
+        if PlayerInput.ScrollWheelSpeed < 0 then
+            globalzoomNew=math.max(globalzoomMin,globalzoomNew*(1-zoomSpeed))
+        end
+        if PlayerInput.ScrollWheelSpeed > 0 then
+            globalzoomNew=math.min(globalzoomMax,globalzoomNew*(1+zoomSpeed))
+        end
+        if PlayerInput.KeyDown(resetKey) then
+            globalzoomNew=globalzoomDefault*zoomStart
+        end
+        ptable.cam.globalZoomScale = globalzoomNew or globalzoomDefault
+    else
+        ptable.cam.globalZoomScale = globalzoomDefault --reset to def
+    end
+end,Hook.HookMethodType.After)
 
+
+--Original code by zomgtehderpy
+--From communtiy mod https://steamcommunity.com/sharedfiles/filedetails/?id=2817020588
+--May conflict with original mod
